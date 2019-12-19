5bffccfe81a5bf78ef1365337c6d9049d62179cf
nguyenhuutukhtn <nguyenhuutu.khtn@gmail.com>
2019-12-19 15:18:12 +0700

    edit ui history tutor

--- src/components/tutor/TutorDetail.js
+++ src/components/tutor/TutorDetail.js
@@ -6,48 +6,66 @@ import TutorHistory from './TutorHistory';
 import TutorInfo from './TutorInfo';
 import ListTutorSkill from './ListTutorSkill';
 import TutorReview from './TutorReview';
-import { requestTutorDetail } from '../../actions/tutor.action'
+import { requestTutorDetail } from '../../actions/tutor.action';
 
 class TutorDetail extends React.Component {
   constructor(props) {
     super(props);
     const { location } = this.props;
-    const id = location.search.split("=")[1];
+    const id = location.search.split('=')[1];
     this.state = {
       tutorId: id,
-      tutorData: null,
+      tutorData: null
     };
   }
 
   componentDidMount = () => {
     const { tutorId } = this.state;
     const { getTutorDetail } = this.props;
-    getTutorDetail(tutorId, (res) => {
+    getTutorDetail(tutorId, res => {
       this.setState({
-        tutorData: res,
-      })
-    })
-  }
+        tutorData: res
+      });
+    });
+  };
 
   render() {
     const { tutorData } = this.state;
     return (
       <Container>
-        <Row>
-          <Col md="4" className="noPadding noMargin tutor-info-col">
-            <TutorInfo tutorData={tutorData ? tutorData.info : null} introduce={tutorData ? tutorData.introduce[0] : null} />
+        <Row className="main-row ">
+          <Col md="4" className="noPadding">
+            <div className="h-100">
+              <Container className="noPadding noMargin tutor-info-container">
+                <Row className=" noPadding noMargin ">
+                  <Col className=" noPadding noMargin ">
+                    <div>
+                      <TutorInfo
+                        tutorData={tutorData ? tutorData.info : null}
+                        introduce={tutorData ? tutorData.introduce[0] : null}
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
           </Col>
 
           <Col md="4" className="noPadding noMargin">
-            <TutorHistory listOldStudent={tutorData ? tutorData.listOldStudent : null} className="history-item" />
+            <TutorHistory
+              listOldStudent={tutorData ? tutorData.listOldStudent : null}
+              className="history-item"
+            />
           </Col>
           <Col md="4" className="noPadding noMargin">
-            <ListTutorSkill tutorSkill={tutorData ? tutorData.skill : null} className="h-100 d-inline-block mw-100 list-tutor-item border border-dark" />
+            <ListTutorSkill tutorSkill={tutorData ? tutorData.skill : null} />
           </Col>
         </Row>
         <Row className="mt-4">
           <Col className="noPadding noMargin" md="12">
-            <TutorReview listRateAndComment={tutorData ? tutorData.rateAndComment : null} />
+            <TutorReview
+              listRateAndComment={tutorData ? tutorData.rateAndComment : null}
+            />
           </Col>
         </Row>
       </Container>
@@ -56,11 +74,10 @@ class TutorDetail extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  getTutorDetail: (id, cb) =>
-    dispatch(requestTutorDetail(id, cb))
+  getTutorDetail: (id, cb) => dispatch(requestTutorDetail(id, cb))
 });
 
 export default connect(
   null,
   mapDispatchToProps
-)(TutorDetail);;
+)(TutorDetail);
--- src/components/tutor/TutorHistory.js
+++ src/components/tutor/TutorHistory.js
@@ -7,6 +7,8 @@ import {
 } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
 import './tutor.css';
 import TutorHistorySkeleton from '../skeleton/TutorHistorySkeleton';
@@ -15,42 +17,90 @@ export default class TutorHistory extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { collapsed: false };
   }
+
+  collapse = () => {
+    this.setState({
+      collapsed: true
+    });
+  };
 
   renderListOldStudent = () => {
     const { listOldStudent } = this.props;
     let listOldStudentElement = [];
     if (listOldStudent) {
-      listOldStudentElement = listOldStudent.map(student => {
-        return (
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work "
-            date={
-              student.complete_date
-                ? `Kết thúc: ${moment(student.complete_date).format(
-                    'DD-MM-YYYY HH:mm'
-                  )}`
-                : 'Kết thúc: '
-            }
-            contentStyle={{}}
-            iconStyle={{
-              background: '#6782a3',
-              color: '#fff'
-            }}
-            icon={<CheckCircleIcon />}
-          >
-            <h6 className="vertical-timeline-element-title text-left">
-              Học sinh: {student.name}
-            </h6>
-            <h6 className="vertical-timeline-element-subtitle text-left">
-              Thời gian thuê: {student.hours_hire} giờ
-            </h6>
-            <p className="text-left">
-              Giá: {student.price / student.hours_hire}K VNĐ/giờ
-            </p>
-          </VerticalTimelineElement>
-        );
+      listOldStudentElement = listOldStudent.map((student, idx) => {
+        if (idx < 4)
+          return (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work "
+              date={
+                student.complete_date
+                  ? `Kết thúc: ${moment(student.complete_date).format(
+                      'DD-MM-YYYY HH:mm'
+                    )}`
+                  : 'Kết thúc: '
+              }
+              contentStyle={{}}
+              iconStyle={{
+                background: '#6782a3',
+                color: '#fff'
+              }}
+              icon={<CheckCircleIcon />}
+            >
+              <h6 className="vertical-timeline-element-title text-left">
+                Học sinh: {student.name}
+              </h6>
+              <h6 className="vertical-timeline-element-subtitle text-left">
+                Thời gian thuê: {student.hours_hire} giờ
+              </h6>
+              <p className="text-left">
+                Giá: {student.price / student.hours_hire}K VNĐ/giờ
+              </p>
+            </VerticalTimelineElement>
+          );
+        return null;
+      });
+    }
+    return listOldStudentElement;
+  };
+
+  renderListOldStudentCollapsed = () => {
+    const { listOldStudent } = this.props;
+    let listOldStudentElement = [];
+    if (listOldStudent) {
+      listOldStudentElement = listOldStudent.map((student, idx) => {
+        if (idx > 3)
+          return (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work "
+              date={
+                student.complete_date
+                  ? `Kết thúc: ${moment(student.complete_date).format(
+                      'DD-MM-YYYY HH:mm'
+                    )}`
+                  : 'Kết thúc: '
+              }
+              contentStyle={{}}
+              iconStyle={{
+                background: '#6782a3',
+                color: '#fff'
+              }}
+              icon={<CheckCircleIcon />}
+            >
+              <h6 className="vertical-timeline-element-title text-left">
+                Học sinh: {student.name}
+              </h6>
+              <h6 className="vertical-timeline-element-subtitle text-left">
+                Thời gian thuê: {student.hours_hire} giờ
+              </h6>
+              <p className="text-left">
+                Giá: {student.price / student.hours_hire}K VNĐ/giờ
+              </p>
+            </VerticalTimelineElement>
+          );
+        return null;
       });
     }
     return listOldStudentElement;
@@ -58,9 +108,10 @@ export default class TutorHistory extends React.Component {
 
   render() {
     const { listOldStudent } = this.props;
+    const { collapsed } = this.state;
     return (
-      <Container className="h-100">
-        <Row className="h-100">
+      <Container className="h-100 history-container">
+        <Row className="list-history-row">
           <Col md="12" className="timeline">
             <div className="ml-sm-auto mr-sm-auto mt-4 history-title">
               Lịch sử dạy học
@@ -69,7 +120,28 @@ export default class TutorHistory extends React.Component {
             <VerticalTimeline animate={false} layout="1-column">
               {listOldStudent ? null : <TutorHistorySkeleton />}
               {this.renderListOldStudent()}
+              {collapsed && this.renderListOldStudentCollapsed()}
             </VerticalTimeline>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {!collapsed && (
+              <Button
+                variant="contained"
+                color="secondary"
+                startIcon={<AddIcon />}
+                style={{
+                  background: '#ffffff',
+                  color: '#6782a3',
+                  marginBottom: '20px',
+                  marginTop: '10px'
+                }}
+                onClick={() => this.collapse()}
+              >
+                Xem thêm
+              </Button>
+            )}
           </Col>
         </Row>
       </Container>
